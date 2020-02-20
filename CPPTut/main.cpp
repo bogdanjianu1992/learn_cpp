09bd9b13cbfdfc55f5fc3f3f093829b99b92188d
bogdanjianu1992 <bogdanjianu06@gmail.com>
2020-02-20 10:40:34 +0000

    added cpptut project to repo

---
+++ CPPTut/main.cpp
@@ -0,0 +1,22 @@
+
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <limits>
+#include <vector>
+#include <sstream>
+#include <numeric>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+    
+    cout << "Hello, world" << endl;
+
+    return 0;
+}
+
